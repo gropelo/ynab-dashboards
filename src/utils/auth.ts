ee273885be95c1bbdec04121ab2bd42aa1a01878
Danilo <danilo.gropelo@icloud.com>
2020-05-01 21:31:21 -0300

    Adding hostname to environment variable

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -10,12 +10,12 @@ const getStorageToken = () =>
 
 const setStorageToken = (token: string) => {
   localStorage.setItem('token', token);
-  window.location.href = 'http://localhost:3000';
+  window.location.href = process.env.REACT_APP_HOSTNAME || 'http://localhost:3000';
 }
 
 export const invalidate = () => {
   localStorage.removeItem('token');
-  window.location.href = 'http://localhost:3000';
+  window.location.href = process.env.REACT_APP_HOSTNAME || 'http://localhost:3000';
 }
 
 const configureAxios = (token: string) => {
@@ -30,7 +30,7 @@ const configureAxios = (token: string) => {
 
 const requestAuthorization = () => {
   const clientId = process.env.REACT_APP_YNAB_CLIENT_ID;
-  window.location.href = `https://app.youneedabudget.com/oauth/authorize?client_id=${clientId}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F%3F&response_type=token`;
+  window.location.href = `https://app.youneedabudget.com/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(process.env.REACT_APP_HOSTNAME || 'http://localhost:3000/')}&response_type=token`;
 }
 
 export const getToken = () => {
