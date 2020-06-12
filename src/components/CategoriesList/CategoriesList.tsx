19a5c89ce17e20701735acb9a9171f98c3a7aa84
Danilo Gropelo <dgropelo@gmail.com>
2020-06-12 15:03:43 -0300

    Remove unecessary check.

--- src/components/CategoriesList/CategoriesList.tsx
+++ src/components/CategoriesList/CategoriesList.tsx
@@ -11,9 +11,9 @@ export const CategoriesList = () => {
   
   return (
     <>
-      <SideNavLink onClick={() => history && history.push('/')} active={location.pathname === '/'}>All Categories</SideNavLink>
+      <SideNavLink onClick={() => history.push('/')} active={location.pathname === '/'}>All Categories</SideNavLink>
       {categories.map(c => 
-        <SideNavLink key={c.id} active={`/categories/${c.id}` === location.pathname} onClick={() => history && history.push(`/categories/${c.id}`)}>{c.name}</SideNavLink>
+        <SideNavLink key={c.id} active={`/categories/${c.id}` === location.pathname} onClick={() => history.push(`/categories/${c.id}`)}>{c.name}</SideNavLink>
       )}
     </>
   )
